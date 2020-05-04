59fd45cadddabca73517456c849e82bc84e62f61
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2020-05-04 15:12:37 -0400

    504 1412 update

--- cars-value-predictor.py
+++ cars-value-predictor.py
@@ -59,42 +59,4 @@ for number_of_page in range(360, 540):
 years = fst_page_years + rest_page_years1 + rest_page_years2 + rest_page_years3
 
 
-#MILEAGE SCRAPING
-
-fst_page_listings_miles = page1_listings_soup.find_all('div', {'class': 'font-size-1 text-truncate'})
-fst_page_miles = re.findall('g>[0-9]*,[0-9]+', str(fst_page_listings_miles))
-fst_page_mileage = list(map(lambda mileage: mileage[2:], fst_page_miles))
-
-
-def pagemileage(number):
-    nth_listings_soup_miles = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=' + str(number)).content, 'lxml')
-    nth_page_listings_miles = page1_listings_soup.find_all('div', {'class': 'font-size-1 text-truncate'})
-    nth_page_miles = re.findall('g>[0-9]*,[0-9]+', str(fst_page_listings_miles))
-    return nth_page_miles
-
-
-rest_page_mileage1 = []
-rest_page_mileage2 = []
-rest_page_mileage3 = []
-
-
-for number_of_page in range(2, 180):
-    rest_page_mileage1 += pagemileage(number_of_page)
-
-
-for number_of_page in range(180, 360):
-    rest_page_mileage2 += pagemileage(number_of_page)
-
-
-for number_of_page in range(360, 540):
-    rest_page_mileage3 += pagemileage(number_of_page)
-
-
-page_mileage = fst_page_mileage + rest_page_mileage1 + rest_page_mileage2 + rest_page_mileage3
-mileage = list(map(lambda miles: miles[2:], page_mileage))
-
-
-#EXTERIOR COLOR
-
-
 
