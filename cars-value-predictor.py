6112ce8b447ff04e460ea9da5ea578df3d15280f
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2020-05-05 00:00:49 -0400

    504 2300 update

--- cars-value-predictor.py
+++ cars-value-predictor.py
@@ -22,41 +22,60 @@ from sklearn.metrics import roc_curve, roc_auc_score
 #PRICES SCRAPING
 
 fst_page_listings_soup = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/').content, 'lxml')
+fst_page_prices_soup = fst_page_listings_soup.find_all('h4', {'data-test': 'vehicleCardPricingBlockPrice'})
+fst_page_prices = re.findall('[0-9]+,[0-9]+', str(fst_page_prices_soup))
 
 
+#Scraping rest of pages listings prices
+
+def pricesscraper(number_of_page):
+    nth_page_listings_soup = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=' + str(number_of_page)).content, 'lxml')
+    nth_page_prices_soup = nth_page_listings_soup.find_all('h4', {'data-test': 'vehicleCardPricingBlockPrice'})
+    nth_page_prices = re.findall('[0-9]+,[0-9]+', str(nth_page_prices_soup))
+    return nth_page_prices
+
+
+prices = list(map(pricesscraper, range(2, 600)))
+prices = str(prices).replace('[', '').replace(']', '').split(', ')
+prices = list(map(lambda x: x[1:-1], prices))
+prices += fst_page_prices
 
 
 #YEARS SCRAPING
 
-fst_page_listings_years = fst_page_listings_soup.find_all('span', {'class': 'vehicle-card-year'})
-fst_page_years = list(re.findall('[12][0-9]{3}', str(fst_page_listings_years)))
+fst_page_years_soup = fst_page_listings_soup.find_all('span', {'class': 'vehicle-card-year'})
+fst_page_years = list(re.findall('[12][0-9]{3}', str(fst_page_years_soup)))
 
 
-def pageyears(number):
-    nth_page_listings_soup = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=' + str(number)).content, 'lxml')
-    nth_page_listings = nth_page_listings_soup.find_all('span', {'class': 'vehicle-card-year'})
-    nth_page_years = list(re.findall('[12][0-9]{3}', str(nth_page_listings)))
+def yearsscraper(number_of_page):
+    nth_page_listings_soup = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=' + str(number_of_page)).content, 'lxml')
+    nth_page_years_soup = nth_page_listings_soup.find_all('span', {'class': 'vehicle-card-year'})
+    nth_page_years = re.findall('[12][0-9]{3}', str(nth_page_years_soup))
     return nth_page_years
 
 
-rest_page_years1 = []
-rest_page_years2 = []
-rest_page_years3 = []
-
-#Splitting in three loops had to be done in order to speed up the process.
-for number_of_page in range(2, 180):
-    rest_page_years1 += pageyears(number_of_page)
+years = list(map(yearsscraper, range(2, 600)))
+years = str(years).replace('[', '').replace(']', '').split(', ')
+years = list(map(lambda x: x[1:-1], years))
+years += fst_page_years
 
 
-for number_of_page in range(180, 360):
-    rest_page_years2 += pageyears(number_of_page)
+#MILEAGES SCRAPING
+
+fst_page_listings_mileages = fst_page_listings_soup.find_all('div', {'class': 'font-size-1 text-truncate'})
+fst_page_mileages_soup = re.findall('g>[0-9]*,[0-9]+', str(fst_page_listings_mileages))
+fst_page_mileages = list(map(lambda mileage: mileage[2:], fst_page_mileages_soup))
 
 
-for number_of_page in range(360, 540):
-    rest_page_years3 += pageyears(number_of_page)
+def mileagesscraper(number):
+    nth_page_listings_soup = BeautifulSoup(requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=' + str(number)).content, 'lxml')
+    nth_page_mileages_soup = nth_page_listings_soup.find_all('div', {'class': 'font-size-1 text-truncate'})
+    nth_page_mileages_unf = re.findall('g>[0-9]*,[0-9]+', str(nth_page_mileages_soup))
+    nth_page_mileages = list(map(lambda mileage: mileage[2:], nth_page_mileages_unf))
+    return nth_page_mileages
 
 
-years = fst_page_years + rest_page_years1 + rest_page_years2 + rest_page_years3
-
-
-
+mileages = list(map(mileagesscraper, range(2, 600)))
+mileages = str(mileages).replace('[', '').replace(']', '').split(', ')
+mileages = list(map(lambda x: x[1:-1], mileages))
+mileages += fst_page_mileages
